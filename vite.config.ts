9a6038d31919b8142bd90549db69d66c5b9f9472
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-02-13 16:29:58 -0500

    Actualizar configuración de Vite y agregar verificación de tipos en los scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-  ],
-  server: {
-    port: 3050, // Puerto inicial a usar (3050 por defecto)
-    strictPort: false, // Si el puerto está en uso, busca otro disponible
-  },
+	plugins: [react(), tsconfigPaths()],
+	server: {
+		port: 3055,
+		strictPort: false,
+	},
 });
-
