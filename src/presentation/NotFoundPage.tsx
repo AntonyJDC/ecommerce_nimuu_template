451ba31b5c8cd50c6ea57450a161c1987db02cff
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2025-02-23 19:22:46 -0500

    fix: update notFoundPage to layout page

--- src/presentation/NotFoundPage.tsx
+++ src/presentation/NotFoundPage.tsx
@@ -1,22 +1,22 @@
 const NotFoundPage = () => {
 	return (
-		<main className="grid min-h-dvh w-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+		<main className="grid min-h-dvh bg-base-100 w-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
 			<section>
 				<div className=" py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
 					<div className="mx-auto max-w-screen-sm text-center">
 						<h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 ">
 							404
 						</h1>
-						<p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl">
+						<p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-base-content">
 							Something's missing.
 						</p>
-						<p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+						<p className="mb-4 text-lg font-light text-base-content/60">
 							Sorry, we can't find that page. You'll find lots to explore on the
 							home page.{" "}
 						</p>
 						<a
 							href="/"
-							className="inline-flex bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+							className="inline-flex bg-primary-600 hover:bg-primary hover:text-primary-content focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4"
 						>
 							Back to Homepage
 						</a>
