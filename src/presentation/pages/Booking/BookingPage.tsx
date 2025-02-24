f624780cf97ddd5d8e8b3a1801b9b2b3673a035d
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2025-02-24 00:05:25 -0500

    feat: add booking page

---
+++ src/presentation/pages/Booking/BookingPage.tsx
@@ -0,0 +1,9 @@
+import { ComingSoonPage } from "@/presentation/ComingSoonPage";
+
+export const BookingPage = () => {
+	return (
+		<div>
+			<ComingSoonPage title="Book appointment" />
+		</div>
+	);
+};
