d9bf04ea49246952c7f15f8716e2beab71a163a5
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2025-03-10 19:24:49 -0500

    feat: add price range filtering to Filters component with apply functionality

--- src/presentation/components/shop-page/filters/index.tsx
+++ src/presentation/components/shop-page/filters/index.tsx
@@ -1,22 +1,34 @@
+import { useState } from "react";
 import CategoriesSection from "../../../components/shop-page/filters/CategoriesSection";
 import DressStyleSection from "../../../components/shop-page/filters/DressStyleSection";
 import PriceSection from "../../../components/shop-page/filters/PriceSection";
 import SizeSection from "../../../components/shop-page/filters/SizeSection";
 import { Button } from "../../ui/button";
 
-const Filters = () => {
+interface FiltersProps {
+  onFilterChange: (filters: { priceRange: number[] }) => void;
+}
+
+const Filters: React.FC<FiltersProps> = ({ onFilterChange }) => {
+  const [priceRange, setPriceRange] = useState([0, 50000]);
+
+  const applyFilters = () => {
+    onFilterChange({ priceRange });
+  };
+
   return (
     <>
       <hr className="border-t-base-content/10" />
       <CategoriesSection />
       <hr className="border-t-base-content/10" />
-      <PriceSection />
+      <PriceSection priceRange={priceRange} onChange={setPriceRange} />
       <hr className="border-t-base-content/10" />
       <SizeSection />
       <hr className="border-t-base-content/10" />
       <DressStyleSection />
       <Button
         type="button"
+        onClick={applyFilters}
         className="bg-primary text-primary-content w-full rounded-full text-sm font-medium py-4 h-12"
       >
         Apply Filter
