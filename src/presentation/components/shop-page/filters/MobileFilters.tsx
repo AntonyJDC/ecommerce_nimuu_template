94daf8f93e962d0e4e55e220fe50e95d3fd4a27d
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2025-03-10 19:37:17 -0500

    feat: add onFilterChange prop to MobileFilters component for filter updates

--- src/presentation/components/shop-page/filters/MobileFilters.tsx
+++ src/presentation/components/shop-page/filters/MobileFilters.tsx
@@ -9,7 +9,11 @@ import {
 import { FiSliders } from "react-icons/fi";
 import Filters from ".";
 
-const MobileFilters = () => {
+interface MobileFiltersProps {
+  onFilterChange: (filters: { priceRange: number[] }) => void;
+}
+
+const MobileFilters: React.FC<MobileFiltersProps> = ({ onFilterChange }) => {
   return (
     <>
       <Drawer>
@@ -31,7 +35,7 @@ const MobileFilters = () => {
             <DrawerDescription className="hidden">filters</DrawerDescription>
           </DrawerHeader>
           <div className="max-h-[90%] overflow-y-auto w-full px-5 md:px-6 py-5 space-y-5 md:space-y-6">
-            <Filters />
+            <Filters onFilterChange={onFilterChange} />
           </div>
         </DrawerContent>
       </Drawer>
